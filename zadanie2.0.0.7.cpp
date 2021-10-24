dce7fdde720fe22a5a759fa94112b97b2c0559d0
KubaKniec <91895474+KubaKniec@users.noreply.github.com>
2021-10-24 19:49:39 +0200

    Add files via upload

---
+++ zadanie2.0.0.7.cpp
@@ -0,0 +1,35 @@
+# include <iostream>
+# include <string>
+auto zapytaj ( std :: string const prompt ) -> int
+{
+if ( not prompt . empty ()) {
+std :: cout << prompt ;
+}
+auto value = std :: string {};
+std :: getline ( std :: cin , value );
+return std :: stoi ( value );
+}
+
+using namespace std;
+int suma;
+
+
+auto main() -> int
+{
+        auto const a = zapytaj ("Podaj limit: ");
+        auto const b = zapytaj ("Podaj dzielnik: ");
+
+	for (int i=0; i<=a ;i++)
+	{
+		if(i%b == 0)
+		{
+			suma += i;
+		}
+	}
+
+	std::cout << "Suma liczb wynosi: "<< suma << "\n";
+
+
+
+return 0;
+}
