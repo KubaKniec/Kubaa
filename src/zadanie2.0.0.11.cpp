4c6c4b3aa42ddcb0a280c869951c9790025b72d7
KubaKniec <91895474+KubaKniec@users.noreply.github.com>
2021-10-25 17:59:48 +0200

    Add files via upload

---
+++ src/zadanie2.0.0.11.cpp
@@ -0,0 +1,38 @@
+# include <iostream>
+# include <string>
+auto zapytaj ( std :: string const prompt ) -> int
+{
+if ( not prompt . empty ()) {
+std :: cout << prompt ;
+}
+auto value = std :: string {};
+std :: getline ( std :: cin , value );
+return std :: stoi ( value );
+}
+
+
+
+
+auto main () -> int
+{
+	auto const a=zapytaj("podaj szerokosc: ");
+	auto const b=zapytaj("podaj wysokosc: ");
+
+
+	for(int y=0;y<b;y++)
+	{
+		for(int i=0;i<a;i++)
+			{
+				std::cout<<"*";
+			}
+	std::cout<<"\n";
+	}
+
+
+
+
+return 0;
+}
+
+
+
