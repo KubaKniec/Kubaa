9119ca966f43d8a3ae33ee5c300d7c7e7bc20937
KubaKniec <91895474+KubaKniec@users.noreply.github.com>
2021-12-06 17:49:05 +0100

    Tablice

--- src/zadanie3.1.0.7.cpp
+++ src/zadanie3.1.0.7.cpp
@@ -3,15 +3,23 @@
 
 auto sort_asc(int a[], int n) -> void
 {
-    std::sort(a, a+n);
-
-    for (int i = 0; i < n; ++i)
-        std::cout << a[i] << " ";
+    for(int i=0; i<n; i++)
+    {
+        for(int j=1; j<n-i; j++)
+            if(a[j-1]>a[j])
+            {
+                int temp = a[j];
+                a[j] = a[j-1];
+                a[j-1] = temp;
+            }
+    }
+    for(int i=0; i<n; i++)
+        std::cout<<a[i]<<" ";
 }
 
 auto main() -> int
 {
-int tab[] = { 42 , 9 , -1 , 18 , 59 , 3 , 101 , 31 , 72 , 12 };
-sort_asc(tab,10);
-return 0;
+    int tab[] = { 42, 9, -1, 18, 59, 3, 101, 31, 72, 12 };
+    sort_asc(tab,10);
+    return 0;
 }
